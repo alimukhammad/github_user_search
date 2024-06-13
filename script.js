4e9f7e154a4f4f24fd23c06c20053516d0e49014
sardor <alimukhammad1@gmail.com>
2024-06-12 22:47:28 -0400

    3.1

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ let inputForm = document.getElementById("userInput"); // assign element to input
 let inputBox = document.getElementById("search"); // assign element to inputBox variable
 
 // userGetFunction: api call by taking username as input
-const userGetFunction = (name) => { 
-    axios(api + name).then((response) => { 
-        userCard(response.data); 
-        repoGetFunction(name);
+const userGetFunction = (name) => {  //declate variable, accepts one parameter, => is to define a function 
+    axios(api + name).then((response) => { //axios is used to make HTTP requests to a web server, api + name is the URL, then is a promise that is resolved when the request is successful, response is the data returned by the server
+        userCard(response.data); //userCard function is called with the data returned by the server
+        repoGetFunction(name); //repoGetFunction is called with the name of the user
     }) 
     .catch((err) => { 
         if ( 
@@ -33,8 +33,9 @@ const userGetFunction = (name) => {
     });
 } 
 
+// repoGetFunction: api call to get the repositories of the user
 const repoGetFunction = (name) => { 
-	axios( api + name + "/repos?sort=created").then((response) => { 
+	axios( api + name + "/repos?sort=created").then((response) => { //axios is used to make HTTP requests to a web server, api + name is the URL, then is a promise that is resolved when the request is successful, response is the data returned by the server
         repoCardFunction( response.data);
     }) 
     .catch((err) => { errorFunction( "Problem fetching repos");
@@ -44,7 +45,7 @@ const repoGetFunction = (name) => {
 //userCard: to display all the user information
 const userCard = (user) => { 
 	let id = user.name || user.login; 
-	let info = user.bio ? `<p>${user.bio}</p>`: ""; 
+	let info = user.bio ? `<p>${user.bio}</p>`: ""; //if user.bio is true then info is assigned to user.bio else it is assigned to an empty string
 	let cardElement = ` 
             <div class="card"> 
             <div> 
@@ -54,7 +55,7 @@ const userCard = (user) => {
             </div> 
 
             <div class="user-info"> 
-            <h2>${id}</h2>${info}<ul> 
+            <h2>${id}</h2>${info}<ul>  
             <li>${user.followers} <strong>Followers</strong></li> 
             <li>${user.following} <strong>Following</strong></li> 
             <li>${user.public_repos} <strong>Repos</strong></li> 
@@ -62,8 +63,9 @@ const userCard = (user) => {
             <div id="repos"></div> 
             </div> 
             </div>`; 
+            //create dom element with insert expression to develop the cardElement ${} is a placeholder for the value of the variable, create id='repos' to display the repositories of the user
 
-	main.innerHTML = cardElement
+	main.innerHTML = cardElement //assign cardElement to the innerHTML of the main element
 } 
 
 const errorFunction = (error) => { 
@@ -75,24 +77,25 @@ const errorFunction = (error) => {
 	main.innerHTML = cardHTML
 } 
 
+//repoCardFunction: to display the repositories of the user
 const repoCardFunction = (repos) => { 
-	let reposElement = document.getElementById( "repos");
+	let reposElement = document.getElementById( "repos"); //assign element to reposElement variable
 
-	for (let i = 0; i < 5 && i < repos.length; i++) { 
-		let repo = repos[i]; 
-		let repoEl = document.createElement("a"); 
-		repoEl.classList.add("repo"); 
-		repoEl.href = repo.html_url; 
-		repoEl.target = "_blank"; 
-		repoEl.innerText = repo.name; 
-		reposElement.appendChild(repoEl);}
+	for (let i = 0; i < 5 && i < repos.length; i++) { //for loop to display the first 5 repositories of the user
+		let repo = repos[i];                        //assign the value of the current element of the array to the variable repo
+		let repoEl = document.createElement("a"); //create a element in memory assign it to repoEl
+		repoEl.classList.add("repo"); //add class repo to the repoEl element
+		repoEl.href = repo.html_url; //assign the value of the html_url property of the repo object to the href attribute of the repoEl element
+		repoEl.target = "_blank"; //assign the value of the string "_blank" to the target attribute of the repoEl element
+		repoEl.innerText = repo.name; //assign the value of the name property of the repo object to the innerText attribute of the repoEl element because the innerText attribute is a string, it is displayed as text on the screen
+		reposElement.appendChild(repoEl);} //add the repoEl element to the reposElement element because the appendChild() method adds a node to the end of the list of children of a specified parent node
     }
 
-inputForm.addEventListener("submit", (e) => { 
-	e.preventDefault(); 
+inputForm.addEventListener("submit", (e) => { //add an event listener to the inputForm element that listens for the submit event and executes the function when the event occurs
+	e.preventDefault(); //prevent the default action of the event from occurring
 	let user = inputBox.value; 
-	if (user) { 
-		userGetFunction(user); 
-		inputBox.value = "";
+	if (user) { //if the user variable is not empty
+		userGetFunction(user); //call the userGetFunction with the user variable as an argument
+		inputBox.value = ""; //assign an empty string to the value attribute of the inputBox element
     }    
 });
